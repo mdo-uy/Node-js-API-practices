26275154bb538113f8efe5d06dc7528b53190200
Mauricio De Oliveira <mauricio.d.oliveira1@gmail.com>
2020-07-06 00:08:27 -0300

    Delete versions

--- API/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/config')();
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
--- API/startup/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = function() {
-    mongoose.connect('mongodb://localhost/ecommerce')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connected to MongoDB...'));
-} 
